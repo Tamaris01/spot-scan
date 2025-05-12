14567cf1a9040b29085e0f4a5f13af27e9d946b6
Tamaris01 <115511382+Tamaris01@users.noreply.github.com>
2025-05-12 18:39:42 +0700

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -3,33 +3,31 @@
 return [
 
     /*
-    |--------------------------------------------------------------------------
-    | Default Filesystem Disk
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------------------- 
+    | Default Filesystem Disk 
+    |-------------------------------------------------------------------------- 
     |
-    | Here you may specify the default filesystem disk that should be used
-    | by the framework. The "local" disk, as well as a variety of cloud
+    | Here you may specify the default filesystem disk that should be used 
+    | by the framework. The "local" disk, as well as a variety of cloud 
     | based disks are available to your application. Just store away!
-    |
+    | 
     */
-
     'default' => env('FILESYSTEM_DISK', 'local'),
 
     /*
-    |--------------------------------------------------------------------------
-    | Filesystem Disks
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------------------- 
+    | Filesystem Disks 
+    |-------------------------------------------------------------------------- 
     |
-    | Here you may configure as many filesystem "disks" as you wish, and you
-    | may even configure multiple disks of the same driver. Defaults have
+    | Here you may configure as many filesystem "disks" as you wish, and you 
+    | may even configure multiple disks of the same driver. Defaults have 
     | been set up for each driver as an example of the required values.
     |
-    | Supported Drivers: "local", "ftp", "sftp", "s3"
+    | Supported Drivers: "local", "ftp", "sftp", "s3", "cloudinary"
     |
     */
-
     'disks' => [
-
+        
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
@@ -56,27 +54,23 @@ return [
             'throw' => false,
         ],
 
-    'cloudinary' => [
-    'driver' => 'cloudinary',
-    'url' => env('CLOUDINARY_URL'),
-],
-
-
+        'cloudinary' => [
+            'driver' => 'cloudinary',
+            'url' => env('CLOUDINARY_URL'),  // make sure this is set in .env
+        ],
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Symbolic Links
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------------------- 
+    | Symbolic Links 
+    |-------------------------------------------------------------------------- 
     |
-    | Here you may configure the symbolic links that will be created when the
-    | `storage:link` Artisan command is executed. The array keys should be
+    | Here you may configure the symbolic links that will be created when the 
+    | `storage:link` Artisan command is executed. The array keys should be 
     | the locations of the links and the values should be their targets.
     |
     */
-
     'links' => [
         public_path('storage') => storage_path('app/public'),
     ],
-
 ];
